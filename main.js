07513e24fb59bd395d14ecf681a6d56024e5999b
Kuba-Urdzhar <kumar151119903343@gmail.com>
2021-02-13 19:42:57 +0600

    many new changes

---
+++ main.js
@@ -0,0 +1,30 @@
+
+    let burger__menu = document.getElementById('burger_menu')
+
+function burgerMenu(){
+
+    let burger_icon = document.querySelector('.burger_icon')
+    let close_icon = document.getElementById('close_icon')
+
+    burger_icon.addEventListener('click', toggleBurgerMenu)
+
+    close_icon.addEventListener('click', toggleBurgerMenu)
+}
+
+function toggleBurgerMenu() {
+    burger_menu.classList.toggle('burger_menu_active')
+}
+
+burgerMenu() 
+
+
+let buyButtons = document.querySelectorAll('.products__btn')
+let cart_count = document.getElementById('cart_count')
+
+function addToCart() {
+    cart_count.innerText = Number(cart_count.innerText) + 1
+}
+
+for(let i = 0; i < buyButtons.length; i ++) {
+    buyButtons[i].addEventListener('click', addToCart)
+}
